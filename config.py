dae31eaba93e525df6d8bbebe96b0f8647d8e6f7
Judit Szarvas <jusz@dtu.dk>
2018-09-13 13:45:35 +0000

    config file

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.7
+
+### DIRECTORIES
+## temporary dirs' folder
+# default: /tmp
+TMP_FOLDER = "/tmp"
+
+# numpy memmap folder
+# default: /dev/shm
+NUMPY_MEMMAP_DIR = "/dev/shm"
